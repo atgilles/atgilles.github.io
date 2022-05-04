0bc2c64b67ca37bd75fd133d19775c2f61911cd0
Alexis <agilles@normalesup.org>
2022-05-04 20:18:39 +0200

    update

--- build-site.el
+++ build-site.el
@@ -18,21 +18,41 @@
 (setq org-html-validation-link nil
       org-html-head-include-scripts nil
       org-html-head-include-default-style nil
-      org-html-head "<link rel=\"stylesheet\" href=\"https://cdn.simplecss.org/simple.min.css\" />")
+      org-html-head "<link rel=\"stylesheet\" href=\"./other/style.css\" />")
 
 ;; Define the publishing project
 (setq org-publish-project-alist
-      (list
-       (list "my-org-site"
-             :recursive t
-             :base-directory "./content"
-             :publishing-directory "./public"
-             :publishing-function 'org-html-publish-to-html
-             :with-author nil
-             :with-creator nil
-             :with-toc nil
-             :section-numbers nil
-             :time-stamp-file nil)))
+      '(("orgfiles"
+         :recursive t
+         :base-directory "./content"
+         :base-extension "org"
+         :publishing-directory "./public"
+         :publishing-function org-html-publish-to-html
+         :html-preamble t
+         :with-author nil
+         :with-creator nil
+         :with-toc t
+         :section-numbers nil
+         :time-stamp-file nil)
+
+        ("htmlfiles"
+         :recursive t
+         :base-directory "./content"
+         :base-extension "html\\|htm"
+         :publishing-directory "./public"
+         :publishing-function org-publish-attachment)
+
+        ("images"
+         :base-directory "./content/images"
+         :base-extension "jpg\\|gif\\|png"
+         :publishing-directory "./public/images"
+         :publishing-function org-publish-attachment)
+
+        ("other"
+         :base-directory "./content/other"
+         :base-extension "css"
+         :publishing-directory "./public/other"
+         :publishing-function org-publish-attachment)))
 
 ;; Generate the site output
 (org-publish-all t)
